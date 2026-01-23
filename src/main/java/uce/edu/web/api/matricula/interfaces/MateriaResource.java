851e493f3c7ab4e2c8d6cc2f3b95730e86aeef8a
MykeCv <mykecevallos@gmail.com>
2026-01-23 10:08:00 -0500

    Tarea 5

--- src/main/java/uce/edu/web/api/matricula/interfaces/MateriaResource.java
+++ src/main/java/uce/edu/web/api/matricula/interfaces/MateriaResource.java
@@ -10,6 +10,9 @@ import jakarta.ws.rs.POST;
 import jakarta.ws.rs.PUT;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.PathParam;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.Response;
 import uce.edu.web.api.matricula.application.MateriaService;
 import uce.edu.web.api.matricula.domain.Materia;
 
@@ -21,18 +24,21 @@ public class MateriaResource {
 
     @GET
     @Path("")
+    @Produces(MediaType.APPLICATION_JSON)
     public List<Materia> obtenerTodas() {
         return materiaService.listarTodas();
     }
 
     @POST
     @Path("")
-    public void crear(Materia materia) {
-        materiaService.crear(materia);
+    public Response guardar(Materia materia) {
+        this.materiaService.crear(materia);
+        return Response.status(Response.Status.CREATED).entity(materia).build();
     }
 
     @GET
     @Path("/{id}")
+    @Produces(MediaType.APPLICATION_XML)
     public Materia consultarPorId(@PathParam("id") Integer id) {
         return materiaService.consultarPorId(id);
     }
@@ -51,17 +57,19 @@ public class MateriaResource {
     
     @PUT
     @Path("/{id}")
-    public void actualizar(@PathParam("id") Integer id, Materia materia) {
-        materiaService.actualizar(id, materia);
+    public Response actualizar(@PathParam("id") Integer id, Materia materia) {
+        this.materiaService.actualizar(id, materia);
+        return Response.status(210).entity(null).build();
     }
 
 
     @PATCH
     @Path("/{id}/{creditos}")
-    public void actualizarCreditos(
+    public Response actualizarCreditos(
             @PathParam("id") Integer id,
             @PathParam("creditos") Integer creditos) {
-        materiaService.actualizarCreditos(id, creditos);
+        this.materiaService.actualizarCreditos(id, creditos);
+        return Response.status(211).entity(null).build();
     }
 
     @DELETE
